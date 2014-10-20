0b73647a22ea30e0a0360a3817cd760de7531187
Karl-Philipp Richter <richter@richtercloud.de>
2014-10-20 23:43:42 +0200

    implemented from-largest-to-smallest search; tested successfully with test sparse file

--- zfs_search.py
+++ zfs_search.py
@@ -22,12 +22,12 @@ zpool = "zpool"
 truncate = "truncate"
 
 # buffer size in bytes
-buffer_size_default=4096
+buffer_size_default=16*1024*1024
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 zfs_start_bytes = "0x"+"0"*32688+"117a0cb17ada1002" # guessed from <tt>head -c 64K /path/to/sparse_file_vdev | hexdump -C</tt> and dd if=/dev/sdxY bs=512 count=1024 | hexdump -C</tt>
     #"0x"+"00"*808+"117a0cb17ada1002"
-zfs_end_bytes = "0x"+"00"*2048+"117a0cb17ada1002"
+zfs_end_bytes = "0x"+"00"*800+"117a0cb17ada1002"
     #+ "2a819478644bce513fc5f9fbd32e703c65c90d685e1d5d27d4543f5ec92afb98" #
 # the number of bytes following an occurance of zfs_end_bytes without an identifiable pattern
 zfs_end_tail_bytes_count = 16*16*4
@@ -59,12 +59,12 @@ def zfs_search(search_file_path, buffer_size=buffer_size_default, keep_test_file
     # Can initialise from files, bytes, etc.
     s = ConstBitStream(search_bytes) # if argument name is omitted, auto is used
     starts = sorted(list(s.findall(zfs_start_bytes, bytealigned=True))) # ConstBitStream.find returns the bit position (rather than byte position)
-    ends = sorted(list(s.findall(zfs_end_bytes, bytealigned=True)))
+    ends = sorted(list(s.findall(zfs_end_bytes, bytealigned=True)), reverse=True) # reverse causes testing from largest possible to smallest possible (this eventually causes I/O overhead, but possibility to find a pool in the largest set is much higher)
     
     # create test files in a temporary directory because there might thousands and deletion and overwriting shouldn't be necessary to be checked for no advantage
     temp_dir = tempfile.mkdtemp()
     logger.info("using temporary directory '%s'" % (temp_dir,))
-    test_file_name_prefix = tempfile.mkstemp(dir=temp_dir)[1]
+    test_file_name_prefix = os.path.basename(search_file_path)
     for start in starts:
         start = start/8
         for end in ends:
@@ -73,38 +73,33 @@ def zfs_search(search_file_path, buffer_size=buffer_size_default, keep_test_file
                 logger.info("skipping occurance of end at '%s' before start at '%s'" % (str(end),str(start)))
                 continue
             end = end + zfs_end_tail_bytes_count
-            logger.info("creating file from byte %s to byte %s" % (str(start), str(end)))
             test_file_name = "%s-%s-%s" % (test_file_name_prefix, str(start), str(end))
-            # use dd for copying in order to avoid separate handling of block devices and files (might be easy though, simply not researched at the moment); dd doesn't fail if invalid value for skip is specified which makes debugging extremely annoying, asked http://unix.stackexchange.com/questions/163026/how-to-make-dd-fail-if-it-invalid-skip-value-is-specified for input; dd prints normal output to stderr which is neither useful nor intuitive
-            # copy a head and tail with bs=1 and the middle part with buffer_size for performance reasons and dd compatibility
-            sp.check_call([truncate, "-s", str(end-start), test_file_name])
-            count_head = -1
-            if end-start > buffer_size:            
-                count_head = buffer_size-((end-start) % buffer_size)
-            else:
-                count_head = end-start
-            if count_head > 0:
-                sp.check_call([dd, "if=%s" % (search_file_path,), "of=%s" % (test_file_name,), "bs=1", "count=%s" % (str(count_head),), "skip=%s" % (str(start),), ], stdout=sp.PIPE, stderr=sp.PIPE)
-            # copy with buffer_size for performance
-            count = 0
-            if (end-start-count_head) > buffer_size:
-                count = (end-start-count_head) / buffer_size
-                sp.check_call([dd, "if=%s" % (search_file_path,), "of=%s" % (test_file_name,), "bs=%s" % (str(buffer_size)), "count=%s" %(str(count),), "skip=%s" % (str((start+count_head)/buffer_size),), "seek=%s" % (str(count_head),), ], stdout=sp.PIPE, stderr=sp.PIPE)
-            # copy rest
-            count_tail = (end-start-count_head) % buffer_size
-            if count_tail > 0:
-                sp.check_call([dd, "if=%s" % (search_file_path,), "of=%s" % (test_file_name,), "bs=1", "count=%s" % (str(count_tail),), "skip=%s" % (str(start+count_head+count*buffer_size),), "seek=%s" % (str(count_head+count*buffer_size),), ], stdout=sp.PIPE, stderr=sp.PIPE)
+            test_file_path = os.path.join(tempfile.mkdtemp(dir=temp_dir), test_file_name) # put separate directory in order to avoid confusing of zpool import which will only display the first hit matching the name
+            logger.info("creating file %s from byte %s to byte %s" % (test_file_path, str(start), str(end)))
+            
+            search_file = open(search_file_path, "r")
+            search_file.seek(start)
+            test_file = open(test_file_path, "w")
+            write_buffer = None
+            while write_buffer != '':
+                write_buffer = search_file.read(buffer_size)
+                test_file.write(write_buffer)
+            test_file.flush()
+            search_file.close()
+            test_file.close()
             
             # test zpool import
-            zpool_import_process = sp.Popen([zpool, "import", "-d", os.path.realpath(os.path.join(test_file_name, "..")), "-D", ]) # there's no difference in returncode between no results and successful listing of possible import -> parse output
+            zpool_import_process = sp.Popen([zpool, "import", "-d", os.path.realpath(os.path.join(test_file_path, "..")), "-D", ], stdout=sp.PIPE, stderr=sp.PIPE) # there's no difference in returncode between no results and successful listing of possible import -> parse output
             zpool_import_process.wait()
-            zpool_import_output = zpool_import_process.communicate()[1] # no pool available message is written onto stderr
-            if (zpool_import_output is None or zpool_import_output.strip() != "no pools available to import") and zpool_import_process.returncode == 0:
-                logger.info("interval %s to %s possibly contains a valid zpool. The output of the ''zpool import'' test command is:\n%s\n\nThe file which succeeded is '%s'. Skipping further search" % (str(start), str(end), zpool_import_output, test_file_name))
+            zpool_import_output_tuple = zpool_import_process.communicate()
+            zpool_import_output = zpool_import_output_tuple[1] # no pool available message is written onto stderr; subprocess.Popen.communicate can be invoked only once, second invokation causes error because performing I/O on closed file
+            zpool_import_output_stdout = zpool_import_output_tuple[0]
+            if zpool_import_process.returncode == 0 and (zpool_import_output is None or (zpool_import_output.strip() != "no pools available to import" and not "state: UNAVAIL" in zpool_import_output)) and (zpool_import_output_stdout is None or (not "state: UNAVAIL" in zpool_import_output and not "state: UNAVAIL" in zpool_import_output_stdout)): # zpool import returncodes are useless (finding a pool with status UNAVAIL returns 0) 
+                logger.info("interval %s to %s possibly contains a valid zpool. The output of the ''zpool import'' test command is:\n%s\n\n\n%s\n\nThe file which succeeded is '%s'. Skipping further search" % (str(start), str(end), zpool_import_output, zpool_import_output_stdout, test_file_path))
                 return
             else:
                 if not keep_test_files:
-                    os.remove(test_file_name)
+                    os.remove(test_file_path)
     
     return
     
